1d5f10e898dca116827eba3b0fbd115f64c664fd
Adam Miller <amiller.astro@gmail.com>
2020-02-14 11:03:46 -0600

    BUG: arrays not pd series (i.e. AAM is dumb)

--- sn_collision/get_filter_trans.py
+++ sn_collision/get_filter_trans.py
@@ -9,39 +9,39 @@ def get_uvw2_tc():
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("uvw2", filt_df.wavelength, filt_df.transmission)
+    return Filter("uvw2", filt_df.wavelength.values, filt_df.transmission.values)
     
 def get_uvm2_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/UVM2_synphot.txt', 
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("uvm2", filt_df.wavelength, filt_df.transmission)
+    return Filter("uvm2", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_uvw1_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/UVW1_synphot.txt', 
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("uvw1", filt_df.wavelength, filt_df.transmission)
+    return Filter("uvw1", filt_df.wavelength.values, filt_df.transmission.values)
     
 def get_u_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/U_UVOT_synphot.txt', 
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("u_uvot", filt_df.wavelength, filt_df.transmission)
+    return Filter("u_uvot", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_b_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/B_UVOT_synphot.txt', 
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("b_uvot", filt_df.wavelength, filt_df.transmission)
+    return Filter("b_uvot", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_v_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/V_UVOT_synphot.txt', 
                           delim_whitespace=True, skiprows=[0], 
                           header=None, names = ['wavelength', 'transmission']
                           )
-    return Filter("v_uvot", filt_df.wavelength, filt_df.transmission)
+    return Filter("v_uvot", filt_df.wavelength.values, filt_df.transmission.values)
