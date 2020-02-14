797f9ae678be33eef9470f324f5d03aad4fa9351
Adam Miller <amiller.astro@gmail.com>
2020-02-14 14:02:00 -0600

    ENH: add ZTF filters

--- sn_collision/get_filter_trans.py
+++ sn_collision/get_filter_trans.py
@@ -6,42 +6,63 @@ dirname = os.path.dirname(__file__)
 
 def get_uvw2_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/UVW2_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("uvw2", filt_df.wavelength.values, filt_df.transmission.values)
     
 def get_uvm2_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/UVM2_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("uvm2", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_uvw1_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/UVW1_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("uvw1", filt_df.wavelength.values, filt_df.transmission.values)
     
 def get_u_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/U_UVOT_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("u_uvot", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_b_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/B_UVOT_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("b_uvot", filt_df.wavelength.values, filt_df.transmission.values)
 
 def get_v_uvot_tc():
     filt_df = pd.read_csv(dirname + '/filter_data/UVOT/V_UVOT_synphot.txt', 
-                          delim_whitespace=True, skiprows=[0], 
+                          delim_whitespace=True, 
                           header=None, names = ['wavelength', 'transmission']
                           )
     return Filter("v_uvot", filt_df.wavelength.values, filt_df.transmission.values)
+
+def get_g_ztf_tc():
+    filt_df = pd.read_csv(dirname + '/filter_data/ZTF/P48_g.dat', 
+                          delim_whitespace=True, skiprows=[0], 
+                          header=None, names = ['wavelength', 'transmission']
+                          )
+    return Filter("g_ztf", filt_df.wavelength.values, filt_df.transmission.values)
+
+def get_r_ztf_tc():
+    filt_df = pd.read_csv(dirname + '/filter_data/ZTF/P48_r.dat', 
+                          delim_whitespace=True, skiprows=[0], 
+                          header=None, names = ['wavelength', 'transmission']
+                          )
+    return Filter("r_ztf", filt_df.wavelength.values, filt_df.transmission.values)
+
+def get_i_ztf_tc():
+    filt_df = pd.read_csv(dirname + '/filter_data/ZTF/P48_i.dat', 
+                          delim_whitespace=True, skiprows=[0], 
+                          header=None, names = ['wavelength', 'transmission']
+                          )
+    return Filter("i_ztf", filt_df.wavelength.values, filt_df.transmission.values)
